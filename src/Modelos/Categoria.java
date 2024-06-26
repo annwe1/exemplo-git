25b76ec4f676191bfba503f3bafb6c5b0f120dd1
annwe1 <nann3.carol@gmail.com>
2024-06-25 23:34:12 -0300

    API

---
+++ src/Modelos/Categoria.java
@@ -0,0 +1,15 @@
+package Modelos;
+
+public class Categoria {
+    private String nome;
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return nome;
+    }
+
+}
+
